d1ed76f800781341ab6de2655ae5affcbdd9acab
waffleSheep <zayan.superboy@gmail.com>
2022-09-03 17:37:07 +0800

    ur mother

--- android/app/src/main/java/app/nush/cura/RegistrationPaneTwoFragment.kt
+++ android/app/src/main/java/app/nush/cura/RegistrationPaneTwoFragment.kt
@@ -1,16 +1,19 @@
 package app.nush.cura
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import app.nush.cura.databinding.FragmentRegistrationPaneTwoBinding
-import app.nush.cura.databinding.FragmentSecondBinding
-import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+
+
+
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,6 +31,8 @@ class RegistrationPaneTwoFragment : Fragment() {
     private var param2: String? = null
 
     private var _binding: FragmentRegistrationPaneTwoBinding? = null
+
+    
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -47,6 +52,8 @@ class RegistrationPaneTwoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentRegistrationPaneTwoBinding.inflate(inflater, container, false)
+
+        binding.addInterest.setOnClickListener { addNewChip()}
         return binding.root
     }
 
@@ -56,6 +63,46 @@ class RegistrationPaneTwoFragment : Fragment() {
         val adapter = ArrayAdapter(requireContext(), androidx.navigation.ui.R.layout.support_simple_spinner_dropdown_item, items)
         binding?.list?.setAdapter(adapter)
     }
+
+    private fun addNewChip() {
+        val keyword: String = binding.menu.editText?.text.toString()
+
+        // todo add the interests to the user
+        // todo check if interest have been added
+
+        try {
+            val inflater = LayoutInflater.from(this.context)
+
+            // Create a Chip from Layout.
+            val newChip =
+                inflater.inflate(R.layout.layout_chip_entry, binding.chipGroup, false) as Chip
+            newChip.text = keyword
+
+            //
+            // Other methods:
+            //
+            newChip.isCloseIconVisible = true;
+            // newChip.setCloseIconResource(R.drawable.your_icon);
+            // newChip.setChipIconResource(R.drawable.your_icon);
+            // newChip.setChipBackgroundColorResource(R.color.red);
+            // newChip.setTextAppearanceResource(R.style.ChipTextStyle);
+            newChip.elevation = 15F;
+
+            binding.chipGroup.addView(newChip)
+
+            // Set Listener for the Chip:
+            newChip.setOnCloseIconClickListener { v -> handleChipCloseIconClicked(v as Chip) }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(this.context, "Error: " + e.message, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun handleChipCloseIconClicked(chip: Chip) {
+        val parent = chip.parent as ChipGroup
+        parent.removeView(chip)
+    }
+
 
     companion object {
         /**
