17997d620bffa697cacc91008907c5dd0398ce69
kai wen <eliyna.carienne@gmail.com>
2022-09-06 18:30:24 +0800

    added info pages for home

--- android/app/src/main/java/app/nush/cura/ui/main/HomeAdapter.kt
+++ android/app/src/main/java/app/nush/cura/ui/main/HomeAdapter.kt
@@ -1,10 +1,13 @@
 package app.nush.cura.ui.main
 
+import android.content.res.Resources
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import app.nush.cura.R
 
@@ -13,6 +16,7 @@ class HomeAdapter() : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
     private val images = listOf(R.drawable.phone, R.drawable.cbt, R.drawable.img)
     private val titles = listOf("Hotlines", "Cognitive Behavioural Therapy", "Effective Communication")
     private val details = arrayOf("If you need help, Click for more helpful hotlines", "CBT has been shown to be extremely beneficial to anyone struggling with stress", "Learn about explaining your needs and getting what you want")
+    private val info = arrayOf(R.string.hotlines, R.string.cbt, R.string.effective_communication)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
@@ -27,6 +31,19 @@ class HomeAdapter() : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
         holder.imageView.setImageResource(images[position])
         holder.item_title.text = titles[position]
         holder.item_detail.text = details[position]
+
+        holder.itemView.setOnClickListener {view ->
+
+
+
+
+                Navigation.findNavController(view).navigate(R.id.homeFragment_to_homeInfoFragment, Bundle().apply { putString("page_title",
+                    titles[position]
+                ); putString("page_info", view.context.resources.getString(info[position]));
+                    putInt("page_img", images[position])
+                })
+            }
+
     }
 
     override fun getItemCount(): Int = titles.size
@@ -35,6 +52,7 @@ class HomeAdapter() : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
         val imageView: ImageView = itemView.findViewById(R.id.imageView3)
         val item_title: TextView = itemView.findViewById(R.id.item_title)
         val item_detail: TextView = itemView.findViewById(R.id.item_detail)
+
     }
 
 }
---
+++ android/app/src/main/java/app/nush/cura/ui/main/HomeInfoFragment.kt
@@ -0,0 +1,33 @@
+package app.nush.cura.ui.main
+
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import app.nush.cura.R
+
+
+class HomeInfoFragment: Fragment()  {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home_info, container, false)
+
+        val title = requireArguments().get("page_title").toString()
+        val info = requireArguments().get("page_info").toString()
+        val img = requireArguments().get("page_img")
+
+        view.findViewById<TextView>(R.id.home_info_title).text = title
+        view.findViewById<TextView>(R.id.home_info_info).text = info
+        view.findViewById<ImageView>(R.id.home_info_image).setImageResource(img as Int);
+
+        return view
+    }
+}
